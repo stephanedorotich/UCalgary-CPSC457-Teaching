c841f30914f5ffb6a2094db4aa334d9fdf57dd7f
Stephane Dorotich <stephanedorotich@gmail.com>
2022-10-19 23:56:41 -0600

    Week 6 Tutorial 2

--- 4-Threads/find_factors.cpp
+++ 4-Threads/find_factors.cpp
@@ -4,33 +4,48 @@
 #include <vector>
 /*
 This program displays every factor pair of each integer from 1 to N.
+Additionally, any integer that has more than 4 factor pairs is added to a vector
 */
 
 void usage_error(const char * pname) {
-    std::cout << "Usage:\n\t" << pname << " n\twhere n is a positive integer.\n";
+    std::cout << "Usage:\n\t" << pname << " n threshold\twhere n is a positive integer.\n";
     exit(-1);
 }
 
-int main(int argc, char ** argv) {
-    if (argc != 2) usage_error(argv[0]);
-    uint64_t n = atoi(argv[1]);
-    if (n == 0) usage_error(argv[0]);
+void display_factor_pairs(const uint64_t & num, const std::vector<std::pair<uint64_t, uint64_t>> & factor_pairs) {
+    // Display all factor pairs of number
+    std::cout << num << ":";
+    for (const auto & fp : factor_pairs) {
+        printf(" (%ld, %ld)", fp.first, fp.second);
+    }
+    std::cout << std::endl;
+}
 
+int main(int argc, char ** argv) {
+    if (argc != 3) usage_error(argv[0]);
+    uint64_t n = atoi(argv[1]);
+    uint64_t x = atoi(argv[2]);
+    if (n == 0 || x == 0) usage_error(argv[0]);
+
+    std::vector<uint64_t> nums_with_x_fp;
     std::vector<std::pair<uint64_t, uint64_t>> factor_pairs;
     for (uint64_t number = 1; number <= n; number++) {
-        // Find all factor pairs of number
-        factor_pairs.clear(); // clear the vector of pairs
+        factor_pairs.clear(); // clear vector
         uint64_t limit = sqrt(number);
 
         for (int factor = 1; factor <= limit; factor++)
             if (number % factor == 0)
                 factor_pairs.push_back({factor, number / factor});
 
-        // Display all factor pairs of number
-        std::cout << number << ":";
-        for (const auto & fp : factor_pairs) {
-            printf(" (%ld, %ld)", fp.first, fp.second);
-        }
-        std::cout << std::endl;
+        // display_factor_pairs(number, factor_pairs);
+        if (factor_pairs.size() >= x)
+            nums_with_x_fp.push_back(number);
     }
+
+    // std::vector<uint64_t> res;
+    // res.insert(std::end(res), std::begin(a), std::end(a));
+
+    for (uint64_t i : nums_with_x_fp)
+        std::cout << i << ", ";
+    std::cout << std::endl;
 }
